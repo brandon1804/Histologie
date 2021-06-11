2c4cc1001aee1aaaa2f5ef12d5744c92f8e6e71b
Michell <19030685@myrp.edu.sg>
2021-06-12 02:07:50 +0800

    Created navbar.php and homepage.php

---
+++ homepage.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Histologie</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/all.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+        <style type="text/css"></style>
+    </head>
+        <body>
+            <?php
+            session_start();
+            include("navbar.php");
+            ?>
+        </body>
+</html>
