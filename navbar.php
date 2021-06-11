df91dbed1b015556b4b963c97a4f305659729331
Michell <19030685@myrp.edu.sg>
2021-06-12 02:19:01 +0800

    Edited navbar.php

--- navbar.php
+++ navbar.php
@@ -1,5 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="home.php"><img src="./css/img/histologie.png" alt="logo" style="width:180px;"></a>
+    <a class="navbar-brand" href="homepage.php"><img src="./css/img/histologie.png" alt="logo" style="width: 200px"></a>
+    
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -14,12 +15,11 @@
                     <a class="nav-link" href="signup.php"><b>Sign up</b></a>
                 </li>
             <?php } else { ?>
-                <li class="nav-item active"><a class="nav-link" style="color: #E11A7A" href="learn.php"><b>Home</b></a></li>
-                <li class="nav-item active"><a class="nav-link" style="color: #E11A7A" href="quiz.php"><b>Quiz</b></a></li>
-                <li class="nav-item active"><a class="nav-link" style="color: #E11A7A" href="images.php"><b>Images</b></a></li>
-                <li class="nav-item active"><a class="nav-link" style="color: #E11A7A" href="profile.php"><b>Profile</b></a></li>
-                <li class="nav-item active"><a class="nav-link" style="color: #E11A7A" href="logout.php"><b>Log Out</b></a></li>
+                <li class="nav-item active"><a class="nav-link" style="color: #E11A7A" href="homepage.php"><b>Home</b></a></li>
+                <li class="nav-item active"><a class="nav-link" style="color: #E11A7A" href=""><b>Quiz</b></a></li>
+                <li class="nav-item active"><a class="nav-link" style="color: #E11A7A" href=""><b>Images</b></a></li>
+                <li class="nav-item active"><a class="nav-link" style="color: #E11A7A" href=""><b>Profile</b></a></li>
+                <li class="nav-item active"><a class="nav-link" style="color: #E11A7A" href=""><b>Log Out</b></a></li>
                 <?php } ?>
         </ul>
     </div>
-</nav>
