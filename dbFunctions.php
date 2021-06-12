c8be4652f39107c46a950af556d2df0200526a8b
brandon1804 <18044455@rp.edu.sg>
2021-06-12 17:25:15 +0800

    Added dbFunctions.php

---
+++ dbFunctions.php
@@ -0,0 +1,13 @@
+<?php
+
+    $username = "root"; 
+    $password = "";        
+    $db       = "histologie";  
+
+
+$host = "localhost";
+$link = mysqli_connect($host,$username,$password,$db) or 
+        die(mysqli_connect_error());
+
+?>
+
