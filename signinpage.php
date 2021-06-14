690113fed913afb4ecc00861c55d09d8fdcab719
Mathias-Andre <19031965@myrp.edu.sg>
2021-06-14 14:57:19 +0800

    signinpage update

---
+++ signinpage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="icon.png">
+        <link rel="stylesheet" href="css/all.css">
+        <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.typekit.net/dte4shr.css">
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
+        <title></title>
+        <style>
+            body{
+                background-image: url('loginBackgroundImage.png')
+            }
+        </style>
+    </head>
+    <body>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
