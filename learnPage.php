6d085f9ffc2dc1feb8058ca1816ec0ece9713979
Mathias-Andre <19031965@myrp.edu.sg>
2021-06-15 17:29:42 +0800

    learnPage test

--- learnPage.php
+++ learnPage.php
@@ -1,8 +1,94 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Histologie</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="icon.png">
+        <link rel="stylesheet" href="css/all.css">
+        <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.typekit.net/dte4shr.css">
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+        <link rel="stylesheet" href="css/gallery-grid.css">
+        <script src="css/gallery-grid.css"></script>
+        <style type="text/css">
+            body{
+                font-family: europa,sans-serif;
+                font-weight: 400;
+                font-style: normal;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+        session_start();
+        include("navbar.php");
+        ?>
+        <div class="container gallery-container">
 
+            <div class="tz-gallery">
+
+                <div class="row">
+                    <div class="col-sm-6 col-md-4">
+                        <a class="lightbox" href="./css/img/capillary.jpg">
+                            <img src="./css/img/capillary.jpg" alt="capillary">
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <a class="lightbox" href="./css/img/cardiacmuscle.jpg">
+                            <img src="./css/img/cardiacmuscle.jpg" alt="cardiacmuscle">
+                        </a>
+                    </div>
+                    <div class="col-sm-12 col-md-4">
+                        <a class="lightbox" href="./css/img/collagenfibers.jpg">
+                            <img src="./css/img/collagenfibers.jpg" alt="collagenfibers">
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <a class="lightbox" href="./css/img/gallbladder.jpg">
+                            <img src="./css/img/gallbladder.jpg" alt="gallbladder">
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <a class="lightbox" href="./css/img/hepatocytes.jpg">
+                            <img src="./css/img/hepatocytes.jpg" alt="hepatocytes">
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <a class="lightbox" href="./css/img/interlobularduct.jpg">
+                            <img src="./css/img/interlobularduct.jpg" alt="interlobularduct">
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <a class="lightbox" href="./css/img/kupffercell.jpg">
+                            <img src="./css/img/kupffercell.jpg" alt="kupffercell">
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <a class="lightbox" href="./css/img/neuromuscularjunction.jpg">
+                            <img src="./css/img/neuromuscularjunction.jpg" alt="neuromuscularjunction">
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <a class="lightbox" href="./css/img/peripheralnerve.jpg">
+                            <img src="./css/img/peripheralnerve.jpg" alt="peripheralnerve">
+                        </a>
+                    </div>
+                    
+                </div>
+
+            </div>
+
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+        <script>
+            baguetteBox.run('.tz-gallery');
+        </script>
+    </body>
+</html>
