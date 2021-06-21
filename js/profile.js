08258286cd964621ca55cf40a3c0eca9205ae80e
anisaaffendi <19030019@rp.edu.sg>
2021-06-21 18:05:21 +0800

    learnPage

---
+++ js/profile.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+    $("#defaultForm").validate({
+        rules: {
+            name: {
+                required: true,
+                pattern: /^[a-zA-Z]* $/
+            },
+            email: {
+                required: true,
+                pattern: /^(\w+[\-\.])*\w+@(\w+\.)+[A-Za-z]+$/
+            },
+            password: {
+                required: true,
+                pattern: /^[A-Za-z\d]{6,8}$/
+            }
+        },
+        messages: {
+            name: {
+                required: "Please enter new name",
+                pattern: "Name must contain only alphabet"
+            },
+            email: {
+                required: "Please enter new email",
+                pattern: "Please enter a valid email"
+            },
+            password: {
+                required: "Please enter new password",
+                pattern: "Password must be 6 to 8 character long"
+            }
+        },
+
+        submitHandler: function () {
+            var name = $("#idName").val();
+            var email = $("#idEmail]").val();
+            var password = $("idPassword").val();
+            $.ajax({
+                url: "doProfile.php",
+                type: "POST",
+                data: "name=" + name + "&email=" + email + "&password=" + password,
+                success: function (data) {
+                     alert("Success");
+                },
+                error: function (obj, textStatus, errorThrown) {
+                    $("#editprofileMsg").html("Unable to update record");
+                    console.log("Error" + textStatus + ":" + errorThrown);
+                    return false;
+                }
+            });
+            return true;
+        }
+    });
+
+}//end of document ready
--- learnPage.php
+++ learnPage.php
@@ -1,15 +1,8 @@
-<?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: http://localhost/Histologie/signinpage.php");
-    exit();
-}//end of user validation
-?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Histologie</title>
-        <meta charset="utf-8">
+        <title>Learn</title>
+       <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/png" href="icon.png">
         <link rel="stylesheet" href="css/all.css">
@@ -19,14 +12,39 @@ if (!isset($_SESSION['user_id'])) {
         <link rel="stylesheet" href="https://use.typekit.net/dte4shr.css">
         <script src="js/jquery.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
-        <link rel="stylesheet" href="css/gallery-grid.css">
-        <script src="css/gallery-grid.css"></script>
         <style type="text/css">
             body{
                 font-family: europa,sans-serif;
                 font-weight: 400;
                 font-style: normal;
+                padding-top: 70px;
+                margin-top: 20px;
+            }
+            .card-text{
+                font-family: europa,sans-serif;
+                font-weight: 700;
+                font-style: normal;
+            }
+            .card{
+                box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+                border-radius: 20px;
+                width: 20rem;
+                height: 20rem;
+                margin-right: 60px;
+            }
+            .card img{
+                width: 20rem;
+                height: 10rem;
+                border-top-left-radius: 20px;
+                border-top-right-radius: 20px;
+            }
+            input[type=text]{
+                float: right;
+                padding: 6px;
+                margin-top: 8px;
+                margin-right: 16px;
+                font-size: 17px;
+                border: 1px solid #ccc;
             }
         </style>
     </head>
@@ -35,66 +53,55 @@ if (!isset($_SESSION['user_id'])) {
         session_start();
         include("navbar.php");
         ?>
-        <div class="container gallery-container">
-
-            <div class="tz-gallery">
-
-                <div class="row">
-                    <div class="col-sm-6 col-md-4">
-                        <a class="lightbox" href="./css/img/capillary.jpg">
-                            <img src="./css/img/capillary.jpg" alt="capillary">
-                        </a>
-                    </div>
-                    <div class="col-sm-6 col-md-4">
-                        <a class="lightbox" href="./css/img/cardiacmuscle.jpg">
-                            <img src="./css/img/cardiacmuscle.jpg" alt="cardiacmuscle">
-                        </a>
-                    </div>
-                    <div class="col-sm-12 col-md-4">
-                        <a class="lightbox" href="./css/img/collagenfibers.jpg">
-                            <img src="./css/img/collagenfibers.jpg" alt="collagenfibers">
-                        </a>
-                    </div>
-                    <div class="col-sm-6 col-md-4">
-                        <a class="lightbox" href="./css/img/gallbladder.jpg">
-                            <img src="./css/img/gallbladder.jpg" alt="gallbladder">
-                        </a>
-                    </div>
-                    <div class="col-sm-6 col-md-4">
-                        <a class="lightbox" href="./css/img/hepatocytes.jpg">
-                            <img src="./css/img/hepatocytes.jpg" alt="hepatocytes">
-                        </a>
-                    </div>
-                    <div class="col-sm-6 col-md-4">
-                        <a class="lightbox" href="./css/img/interlobularduct.jpg">
-                            <img src="./css/img/interlobularduct.jpg" alt="interlobularduct">
-                        </a>
-                    </div>
-                    <div class="col-sm-6 col-md-4">
-                        <a class="lightbox" href="./css/img/kupffercell.jpg">
-                            <img src="./css/img/kupffercell.jpg" alt="kupffercell">
-                        </a>
-                    </div>
-                    <div class="col-sm-6 col-md-4">
-                        <a class="lightbox" href="./css/img/neuromuscularjunction.jpg">
-                            <img src="./css/img/neuromuscularjunction.jpg" alt="neuromuscularjunction">
-                        </a>
-                    </div>
-                    <div class="col-sm-6 col-md-4">
-                        <a class="lightbox" href="./css/img/peripheralnerve.jpg">
-                            <img src="./css/img/peripheralnerve.jpg" alt="peripheralnerve">
-                        </a>
-                    </div>
-                    
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-6">
+                    <h1>Learn</h1>
                 </div>
-
+                <div class="col-6 d-flex justify-content-end align-items-center">
+                    <select class="form-control w-50" id="idLessonCategory">
+                        <option value="">Select Lesson Category</option>
+                        <option value="tissue and cells">Tissues and cells</option>
+                        <option value="recognising diseases">Recognising diseases</option>
+                        <option value="tumours">Tumours</option>
+                    </select>
+                </div>
             </div>
-
+            <div class="mt-5">
+                <h4>Lessons:</h4>
+                <div class="row">
+                    <div class="column">
+                        <div class="card">
+                            <img class="card-img-top" src="img/lung.JPG" />
+                            <div class="card-body">
+                                <h5 class="card-text">Tissue and cell</h5>
+                                <h6>A cell consists of three parts: the cell membrane, the nucleus, between the two, the cytoplasm. </h6>
+                                <a href="quizResultPage.php" class="btn btn-primary">Start lesson</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card">
+                            <img class="card-img-top" src="img/lung.JPG" />
+                            <div class="card-body">
+                                <h5 class="card-text">Recognising Diseases</h5>
+                                <h6>In this lesson, you will try to identify tissues and try to identify the abnormality. </h6>
+                                <a href="quizResultPage.php" class="btn btn-primary">Start lesson</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card">
+                            <img class="card-img-top" src="img/lung.JPG" />
+                            <div class="card-body">
+                                <h5 class="card-text">Tumours</h5>
+                                <h6>Tumours are groups of abnormal cells that forms lumps or growth. Tumours grow and behave differently. </h6>
+                                <a href="quizResultPage.php" class="btn btn-primary">Start lesson</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
-        <script>
-            baguetteBox.run('.tz-gallery');
-        </script>
     </body>
 </html>
