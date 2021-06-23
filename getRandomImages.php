4e310525d8d79ecd491030ae16191e29e3b24afe
Michell <19030685@myrp.edu.sg>
2021-06-23 15:52:48 +0800

    update

--- getRandomImages.php
+++ getRandomImages.php
@@ -2,11 +2,11 @@
 
 include "dbFunctions.php";
 
-$query = "SELECT name FROM image ORDER BY RAND() LIMIT 12";
+$query = "SELECT filename,name FROM image ORDER BY RAND() LIMIT 12";
 $result = mysqli_query($link, $query);
 
 while ($row = mysqli_fetch_assoc($result)) {
-    $images[] = $row["name"];
+    $images[] = ['filename' => $row['filename'], 'name' => $row['name']];
 }
 mysqli_close($link);
 
--- js/randomImages.js
+++ js/randomImages.js
@@ -1,86 +1,25 @@
 $(document).ready(function () {
-$.ajax({
-type: "GET",
+    $.ajax({
+        type: "GET",
         url: "getRandomImages.php",
         cache: false,
         dataType: "JSON",
         success: function (response) {
-        var message = "";
-                console.log(response);
-               
+            var message = "";
+            console.log(response);
+
             response.forEach(i => {
-                 message += "<div class='col-md-6 col-lg-3'>"
-                + `<a class='lightbox' href="./css/img/homepageImg/${i}">`
-                + `<img src="./css/img/homepageImg/${i}" alt='randomImage'>`
-                + "</a></div>";
+                message += "<div class='col-md-6 col-lg-3'>"
+                        + `<a class='lightbox' href='./css/img/homepageImg/${i.filename}'>`
+                        + `<img src='./css/img/homepageImg/${i.filename}' class='image' alt='image'>`
+                        + `<div class='overlay'>`
+                        + `<div class='text'>${i.name}</div>`
+                        + "</div></a></div>";
             });
-                $("#images").html(message);
+            $("#images").html(message);
         },
         error: function (obj, textStatus, errorThrown) {
-        console.log("Error " + textStatus + " : " + errorThrown);
+            console.log("Error " + textStatus + " : " + errorThrown);
         }
+    });
 });
-        });
-        /*
-         <div class="col-md-6 col-lg-3">
-         <a class="lightbox" href="./css/img/homepageImg/capillary.jpg">
-         <img src="./css/img/homepageImg/capillary.jpg" alt="capillary">
-         </a>
-         </div>
-         <div class="col-md-6 col-lg-3">
-         <a class="lightbox" href="./css/img/homepageImg/cardiacmuscle.jpg">
-         <img src="./css/img/homepageImg/cardiacmuscle.jpg" alt="cardiacmuscle">
-         </a>
-         </div>
-         <div class="col-md-6 col-lg-3">
-         <a class="lightbox" href="./css/img/homepageImg/collagenfibers.jpg">
-         <img src="./css/img/homepageImg/collagenfibers.jpg" alt="collagenfibers">
-         </a>
-         </div>
-         <div class="col-md-6 col-lg-3">
-         <a class="lightbox" href="./css/img/homepageImg/gallbladder.jpg">
-         <img src="./css/img/homepageImg/gallbladder.jpg" alt="gallbladder">
-         </a>
-         </div>
-         <div class="col-md-6 col-lg-3">
-         <a class="lightbox" href="./css/img/homepageImg/hepatocytes.jpg">
-         <img src="./css/img/homepageImg/hepatocytes.jpg" alt="hepatocytes">
-         </a>
-         </div>
-         <div class="col-md-6 col-lg-3">
-         <a class="lightbox" href="./css/img/homepageImg/interlobularduct.jpg">
-         <img src="./css/img/homepageImg/interlobularduct.jpg" alt="interlobularduct">
-         </a>
-         </div>
-         <div class="col-md-6 col-lg-3">
-         <a class="lightbox" href="./css/img/homepageImg/kupffercell.jpg">
-         <img src="./css/img/homepageImg/kupffercell.jpg" alt="kupffercell">
-         </a>
-         </div>
-         <div class="col-md-6 col-lg-3">
-         <a class="lightbox" href="./css/img/homepageImg/neuromuscularjunction.jpg">
-         <img src="./css/img/homepageImg/neuromuscularjunction.jpg" alt="neuromuscularjunction">
-         </a>
-         </div>
-         <div class="col-md-6 col-lg-3">
-         <a class="lightbox" href="./css/img/homepageImg/peripheralnerve.jpg">
-         <img src="./css/img/homepageImg/peripheralnerve.jpg" alt="peripheralnerve">
-         </a>
-         </div>
-         <div class="col-md-6 col-lg-3">
-         <a class="lightbox" href="./css/img/homepageImg/kupffercell.jpg">
-         <img src="./css/img/homepageImg/kupffercell.jpg" alt="kupffercell">
-         </a>
-         </div>
-         <div class="col-md-6 col-lg-3">
-         <a class="lightbox" href="./css/img/homepageImg/neuromuscularjunction.jpg">
-         <img src="./css/img/homepageImg/neuromuscularjunction.jpg" alt="neuromuscularjunction">
-         </a>
-         </div>
-         <div class="col-md-6 col-lg-3">
-         <a class="lightbox" href="./css/img/homepageImg/neuromuscularjunction.jpg">
-         <img src="./css/img/homepageImg/neuromuscularjunction.jpg" alt="neuromuscularjunction">
-         </a>
-         </div>        
-         
-         */
