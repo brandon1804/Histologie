bb65a21a6fbd170e537c56bec43a09474fa51103
brandon1804 <18044455@rp.edu.sg>
2021-07-05 16:17:37 +0800

    Updated navbar

--- navbar.php
+++ navbar.php
@@ -1,12 +1,15 @@
+<link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <a class="navbar-brand" href="index.php"><img src="./css/img/histologie.png" alt="logo" style="width: 100px"></a>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <?php if (!isset($_SESSION['user_id'])) { ?>
                 <li class="nav-item active">
                     <a class="nav-link" href="signinpage.php" style="color: #E11A7A"><b>Login</b></a>
