66407a486aeaf94b76842e40e25b199be52fb5c6
Mathias-Andre <19031965@myrp.edu.sg>
2021-07-13 22:08:32 +0800

    password reset email

--- dbFunctions.php
+++ dbFunctions.php
@@ -1,6 +1,5 @@
 <?php
 
-/*
     $username = "root"; 
     $password = "";        
     $db       = "histologie";  
@@ -9,8 +8,6 @@
 $host = "localhost";
 $link = mysqli_connect($host,$username,$password,$db) or 
         die(mysqli_connect_error());
- * 
- */
 
 $connectstr_dbhost = '';
 $connectstr_dbname = '';
@@ -36,5 +33,7 @@ if (!$link) {
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
+*/
+
 ?>
 
