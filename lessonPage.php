14462573a21f0c7e280789bc591c3918e6eaa5de
Michell <19030685@myrp.edu.sg>
2021-07-21 21:22:38 +0800

    update

--- lessonPage.php
+++ lessonPage.php
@@ -18,8 +18,8 @@
         <script src="js/jquery.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>-->
 
-        <!--lesson.js-->
-        <script src="js/lesson.js" type="text/javascript"></script>
+        <!--        lesson.js
+                <script src="js/lesson.js" type="text/javascript"></script>-->
 
         <!--swiper-->
         <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
@@ -59,30 +59,29 @@
                                         </div>-->
 
                     <div id="slider" class="swiper-container swiper-container w-full swiper swiper-container-initialized swiper-container-horizontal swiper-container-pointer-events">
-                        <div class="swiper-wrapper" id="swiper-wrapper-10fc5411034466b78e" aria-live="polite" style="transform: translate3d(-2744px, 0px, 0px); transition-duration: 0ms;">
-<!--                            <div class="swiper-slide" role="group">
-                                <div class="swiper-zoom-container" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px);">
-                                    <img src="./css/img/lessonImage/Lung/slide1.png" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px) scale(1);">
-                                </div>
-                            </div>
-                            <div class="swiper-slide" role="group">
-                                <div class="swiper-zoom-container" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px);">
-                                    <img src="./css/img/lessonImage/Lung/slide2.png" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px) scale(1);">
-                                </div>
-                            </div>-->
+                        <div class="swiper-wrapper" id="swiper-wrapper-10fc5411034466b78e" aria-live="polite" style="transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;">
+                            <!--                            <div class="swiper-slide" role="group">
+                                                            <div class="swiper-zoom-container" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px);">
+                                                                <img src="./css/img/lessonImage/Lung/slide1.png" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px) scale(1);">
+                                                            </div>
+                                                        </div>
+                                                        <div class="swiper-slide" role="group">
+                                                            <div class="swiper-zoom-container" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px);">
+                                                                <img src="./css/img/lessonImage/Lung/slide2.png" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px) scale(1);">
+                                                            </div>
+                                                        </div>-->
                         </div>
                         <div class="swiper-button-next swiper-button-disabled" tabindex="-1" role="button" aria-label="Next slide" aria-controls="swiper-wrapper-10fc5411034466b78e" aria-disabled="true"></div>
                         <div class="swiper-button-prev" tabindex="0" role="button" aria-label="Previous slide" aria-controls="swiper-wrapper-10fc5411034466b78e" aria-disabled="false"></div>
                         <div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets"></div>
                     </div>
-                    <script></script>
                     <div class="flex justify-evenly pt-8">
                         <button class="text-white w-1/2 md:w-full mr-1 bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded-lg text-lg" id="nextLesson">Next Lesson</button>
                         <button class="text-white w-1/2 md:w-full ml-1 bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded-lg text-lg" id="quiz">Take Quiz</button>
                     </div>
                 </div>
             </div>
-            <script>
+<!--            <script>
                 var swiper = new Swiper('.swiper-container', {
                     direction: 'horizontal',
                     zoom: true,
@@ -98,6 +97,8 @@
                         prevEl: '.swiper-button-prev',
                     },
                 })
-            </script>
+            </script>-->
+            <!--lesson.js-->
+            <script src="js/lesson.js" type="text/javascript"></script>
     </body>
 </html>
