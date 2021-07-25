600dc01b5a9fec76dc653af438f5737ae6970704
anisaaffendi <19030019@rp.edu.sg>
2021-07-25 15:21:04 +0800

    getLessonCategory

--- getLessonByCategory.php
+++ getLessonByCategory.php
@@ -7,15 +7,14 @@ $id = $_GET['id'];
 $query = "SELECT L.lesson_id, L.title, L.summary, I.filename FROM lesson L INNER JOIN image I ON L.lesson_id = I.lesson_image_id INNER JOIN lesson_category LC ON L.lesson_category_id = LC.lesson_category_id WHERE LC.lesson_category_id = $id";
 $result = mysqli_query($link, $query);
 
-$row = mysqli_fetch_assoc($result);
-if (!empty($result)){
-    $lessonArr = $row;
+while ($row = mysqli_fetch_assoc($result)) {
+$lessons[] = $row;
 }
 
 mysqli_close($link);
 
 
 
-echo json_encode($lessonArr);
+echo json_encode($lessons);
 ?>
 
