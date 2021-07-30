ba678514c584c29ebe3e2bb4d5699bcefafcaff6
brandon1804 <18044455@rp.edu.sg>
2021-07-30 10:54:49 +0800

    Updated doSignUp.php

--- doSignUp.php
+++ doSignUp.php
@@ -13,35 +13,34 @@ $emailQuery = "SELECT email FROM user WHERE email = '$email'";
 $query = "";
 
 
-// Verification Incomplete.
+
 $select = mysqli_query($link, $emailQuery);
 
-if (mysqli_num_rows($select) > 0){
+if (mysqli_num_rows($select) > 0) {
     $response = "<div class='alert alert-danger text-left' role='alert'>"
-                . "Email already exists"    
-                . "<a href='passwordReset.php'> Click here to reset your password</a></div>";
-        
-        echo $response;
-}else{
-    if ($accountType == "student") {
-            $query = "INSERT INTO user (name, student_id, staff_id, email, password, account_type) VALUES ('$name', $id, NULL, '$email', SHA1('$password'), '$accountType')";    
-        } 
-        else if ($accountType == "staff") {
-            $query = "INSERT INTO user (name, student_id, staff_id, email, password, account_type) VALUES ('$name', NULL, $id, '$email', SHA1('$password'), '$accountType')";
-        }
-}
+            . "Email already exists"
+            . "<a href='passwordReset.php'> Click here to reset your password</a></div>";
 
-$result = mysqli_query($link, $query) or die(mysqli_error($link));
-
-if ($result) {
-    $response = "<div class='alert alert-success text-left' role='alert'>"
-            . "You have signed up successfully"
-            . "<a href='signinPage.php'> Continue</a></div>";
+    echo $response;
 } else {
-    $response = "<div class='alert alert-danger text-left' role='alert'>"
-            . "Sign up failed."
-            . "<a href='signinpage.php'> Try again</a></div>";
-}
+    if ($accountType == "student") {
+        $query = "INSERT INTO user (name, student_id, staff_id, email, password, account_type) VALUES ('$name', $id, NULL, '$email', SHA1('$password'), '$accountType')";
+    } else if ($accountType == "staff") {
+        $query = "INSERT INTO user (name, student_id, staff_id, email, password, account_type) VALUES ('$name', NULL, $id, '$email', SHA1('$password'), '$accountType')";
+    }
 
-echo $response;
+    $result = mysqli_query($link, $query) or die(mysqli_error($link));
+
+    if ($result) {
+        $response = "<div class='alert alert-success text-left' role='alert'>"
+                . "You have signed up successfully"
+                . "<a href='signinPage.php'> Continue</a></div>";
+    } else {
+        $response = "<div class='alert alert-danger text-left' role='alert'>"
+                . "Sign up failed."
+                . "<a href='signinpage.php'> Try again</a></div>";
+    }
+
+    echo $response;
+}
 ?>    
