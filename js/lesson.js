07c5245cd0ded07d4899ac7ed986fe1812f5c557
Michell <19030685@myrp.edu.sg>
2021-08-03 01:03:29 +0800

    update

--- js/lesson.js
+++ js/lesson.js
@@ -32,45 +32,18 @@ $(document).ready(function () {
         window.location.href = "lessonpage.php?lesson_id=" + newID;
     });
 
-//    $.ajax({
-//        type: "GET",
-//        url: "getLessonMaterial.php",
-//        data: `{id: ${id}}`,
-//        cache: false,
-//        dataType: "JSON",
-//        success: function (response) {
-//            var message = "";
-//            console.log(response);
-//            $("#title").html(response['title']);
-//            for (let i = 0; i < response.length; i++){
-//                message += "<div class='swiper-slide'>"
-//                        +  "<img src='./css/img/lessonImage/"+ response['title'] + "/" + response['filename'] +"'>"
-//                        +  "</div>";
-//            };
-//            $("#swiper-wrapper").html(message);
-//        },
-//        error: function (obj, textStatus, errorThrown) {
-//            console.log("Error " + textStatus + " : " + errorThrown);
-//        }
-//    });
     $.get("./getLessonMaterial.php", {
         id: `${id}`
     }, (data) => {
-//      console.log("runs");
-//      console.log(data);
         var message = "";
         $("#title").html(data.title);
         for (let i = 0; i < data.slides.length; i++) {
-//            message += "<div class='swiper-slide'>"
-//                    +  "<img src='./css/img/lessonImage/"+ data.title + "/" + data.slides[i] +"'>"
-//                    +  "</div>";
             message += "<div class='swiper-slide' role='group'>"
                     + "<div class='swiper-zoom-container' style='transition-duration: 300ms; transform: translate3d(0px, 0px, 0px);'>"
                     + "<img src='./css/img/lessonImage/" + data.title + "/" + data.slides[i] + "' style='transition-duration: 300ms; transform: translate3d(0px, 0px, 0px) scale(1);'>"
                     + "</div></div>";
         }
         ;
-//        $("#swiper-wrapper").html(message);
         $("#swiper-wrapper-10fc5411034466b78e").html(message);
     }, "JSON");
 
--- lessonPage.php
+++ lessonPage.php
@@ -3,7 +3,7 @@ session_start();
 if (!isset($_SESSION['user_id'])) {
     header("Location: signinpage.php");
     exit();
-}//end of user validation
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,11 +24,7 @@ if (!isset($_SESSION['user_id'])) {
         <link rel="stylesheet" href="https://use.typekit.net/dte4shr.css">
         <script src="js/jquery.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>-->
-        
-        <!--        lesson.js
-                <script src="js/lesson.js" type="text/javascript"></script>-->
 
-        <!--swiper-->
         <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
         <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
 
@@ -51,32 +47,8 @@ if (!isset($_SESSION['user_id'])) {
 
                     <p class="text-center md:text-4xl font-bold mb-5" id="title"></p>
 
-                    <!--                    <div id="slider" class="swiper-wrapper swiper-container w-full">
-                                            <div class="swiper-wrapper" id="swiper-wrapper">
-                    
-                                            </div>
-                                            <div class="swiper-button-prev"></div>
-                                            <div class="swiper-button-next"></div>
-                                            <div class="swiper-pagination"></div>
-                                        </div>
-                    
-                                        <div class="flex justify-evenly pt-8">
-                                            <button class="text-white w-1/2 md:w-full mr-1 bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded-lg text-lg" id="nextLesson">Next Lesson</button>
-                                            <button class="text-white w-1/2 md:w-full ml-1 bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded-lg text-lg" id="quiz">Take Quiz</button>
-                                        </div>-->
-
                     <div id="slider" class="swiper-container swiper-container w-full swiper swiper-container-initialized swiper-container-horizontal swiper-container-pointer-events">
                         <div class="swiper-wrapper" id="swiper-wrapper-10fc5411034466b78e" aria-live="polite" style="transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;">
-                            <!--                            <div class="swiper-slide" role="group">
-                                                            <div class="swiper-zoom-container" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px);">
-                                                                <img src="./css/img/lessonImage/Lung/slide1.png" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px) scale(1);">
-                                                            </div>
-                                                        </div>
-                                                        <div class="swiper-slide" role="group">
-                                                            <div class="swiper-zoom-container" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px);">
-                                                                <img src="./css/img/lessonImage/Lung/slide2.png" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px) scale(1);">
-                                                            </div>
-                                                        </div>-->
                         </div>
                         <div class="swiper-button-next swiper-button-disabled" tabindex="-1" role="button" aria-label="Next slide" aria-controls="swiper-wrapper-10fc5411034466b78e" aria-disabled="true"></div>
                         <div class="swiper-button-prev" tabindex="0" role="button" aria-label="Previous slide" aria-controls="swiper-wrapper-10fc5411034466b78e" aria-disabled="false"></div>
@@ -88,24 +60,6 @@ if (!isset($_SESSION['user_id'])) {
                     </div>
                 </div>
             </div>
-<!--            <script>
-                var swiper = new Swiper('.swiper-container', {
-                    direction: 'horizontal',
-                    zoom: true,
-                    loop: false,
-
-                    pagination: {
-                        el: ".swiper-pagination",
-                        clickable: true,
-                    },
-
-                    navigation: {
-                        nextEl: '.swiper-button-next',
-                        prevEl: '.swiper-button-prev',
-                    },
-                })
-            </script>-->
-            <!--lesson.js-->
             <script src="js/lesson.js" type="text/javascript"></script>
     </body>
 </html>
