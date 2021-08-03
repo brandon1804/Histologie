e88cff9c351a92b21fba227ca82585ac379a8710
Mathias-Andre <19031965@myrp.edu.sg>
2021-08-03 10:56:58 +0800

    Completed images component with zoom function.

--- js/imgCat.js
+++ js/imgCat.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    reload_Images();
     updateImages();
 });
 
@@ -26,41 +25,6 @@ function updateImages() {
         },
         error: function (obj, textStatus, errorThrown) {
             console.log("Error " + textStatus + ": " + errorThrown);
-        }
-    });
-}
-
-function reload_Images() {
-    $("#idImgCategory").change(function () {
-        var imgCat = $("#idImgCategory").val();
-
-        if (imgCat == 0) {
-            updateLessons();
-        } else {
-            $.ajax({
-                type: "GET",
-                url: "getImgByCategory.php",
-                data: "id=" + imageCat,
-                cache: false,
-                dataType: "JSON",
-                success: function (response) {
-                    var message = "";
-                    for (i = 0; i < response.length; i++) {
-                        message += "<div class='col-sm-12 col-md-6 col-lg-6 col-xl-4'>"
-                                + "<div class='card shadow' style='width: 20rem;' id=" + response[i].image_id + ">"
-                                + "<img class='card-img-top' src='css/img/homepageImg/" + response[i].filename + "' alt='homepageImg'>"
-                                + "<div class='card-body'>"
-                                + "<h5 class='card-title'>" + response[i].name + "</h5>"
-                                + "<a href='specimen.php' class='btn btn-primary'>More Info</a>"
-                                + "</div></div></div>";
-                    }
-                    $("#imgRow").html(message);
-                },
-                error: function (obj, textStatus, errorThrown) {
-                    console.log("Error " + textStatus + ": " + errorThrown);
-                    $("#imgRow").html("");
-                }
-            });
         }
     });
 }
--- js/specimenImg.js
+++ js/specimenImg.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-
+    
     $("form").click(function(e){
         let id = e.currentTarget.id;
         console.log(e);
         $("#" + id).submit();
-
+        
     })
 });
